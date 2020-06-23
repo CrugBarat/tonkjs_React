745427061897ac8c398b8c62b4afe1c1acd842b1
CrugBarat <craigtbarratt@gmail.com>
2020-06-23 04:11:33 +0100

    adds Drum Machine config file from https://github.com/CrugBarat/drum_machine_React
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/config/drum_machine/SampleArray.js
@@ -0,0 +1,13 @@
+import kick from '../assets/sounds/kick.wav';
+import hat from '../assets/sounds/hat.wav';
+import snare from '../assets/sounds/snare.wav';
+import tom from '../assets/sounds/tom.wav';
+
+const SampleArray = [
+                      [{"E5": hat}],
+                      [{"E2": tom}],
+                      [{"E3": snare}],
+                      [{"E1": kick}]
+                    ]
+
+export default SampleArray;
