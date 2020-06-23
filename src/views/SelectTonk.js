66976dc2ced09395c817b768750f4a7a5b053dde
CrugBarat <craigtbarratt@gmail.com>
2020-06-23 13:44:18 +0100

    fixes scrolling bug
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/views/SelectTonk.js
+++ src/views/SelectTonk.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import TonkBoxList from '../components/TonkBoxList';
 import TonkBoxes from '../config/TonkBoxes'
 
 export default function SelectTonk() {
   return (
-    <header>
-      <div className="select-title-container">
-        <h1 className="select-title">TONK.js</h1>
-      </div>
-      <hr/>
+    <Fragment>
+      <header>
+        <div className="select-title-container">
+          <h1 className="select-title">TONK.js</h1>
+        </div>
+        <hr/>
+      </header>
       <main>
         <p className="select-tonk-box">Select your Tonk-box</p>
         <TonkBoxList tonkboxes={TonkBoxes}/>
       </main>
-    </header>
+    </Fragment>
   )
 }
