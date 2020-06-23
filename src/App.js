cb42691f117a7f3f9b1fe89a75bb1f618d8170b8
CrugBarat <craigtbarratt@gmail.com>
2020-06-23 02:23:56 +0100

    sets up router boilerplate
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Home from './views/Home';
+import About from './views/About';
+import SelectWeapon from './views/SelectWeapon';
+import Arpeggiator from './containers/Arpeggiator';
 import './App.css';
 
 function App() {
   return (
-    
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/select">Select Weapon</Link>
+      <Link to="/arpeggiator">Arpeggiator</Link>
+
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/select">
+          <SelectWeapon />
+        </Route>
+        <Route path="/arpeggiator">
+          <Arpeggiator />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/views/SelectWeapon.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SelectWeapon() {
+  return <h2>SELECT WEAPON</h2>;
+}
