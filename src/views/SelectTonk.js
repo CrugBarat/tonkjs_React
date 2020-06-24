73b1bf2d557f2a7c752a3710cabdebcd6d91a508
CrugBarat <craigtbarratt@gmail.com>
2020-06-24 01:14:30 +0100

    adds link logos to the select page
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/views/SelectTonk.js
+++ src/views/SelectTonk.js
@@ -1,6 +1,9 @@
 import React, {Fragment} from 'react';
 import TonkBoxList from '../components/TonkBoxList';
-import TonkBoxes from '../config/TonkBoxes'
+import TonkBoxes from '../config/TonkBoxes';
+import git from '../assets/images/git.png';
+import linked from '../assets/images/linked.png';
+import about from '../assets/images/about.png';
 
 export default function SelectTonk() {
   return (
@@ -14,6 +17,17 @@ export default function SelectTonk() {
       <main>
         <p className="select-tonk-box">Select your Tonk-box</p>
         <TonkBoxList tonkboxes={TonkBoxes}/>
+        <div>
+          <a href="https://github.com/CrugBarat/tonkjs_React">
+            <img className="link-logo" src={git} alt=""/>
+          </a>
+          <a href="https://www.linkedin.com/in/craig-t-barratt/">
+            <img className="link-logo" src={linked} alt=""/>
+          </a>
+          <a href="/about">
+            <img className="link-logo" src={about} alt=""/>
+          </a>
+        </div>
       </main>
     </Fragment>
   )
