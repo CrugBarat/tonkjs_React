c67fc6001c4449d7d6c4ff1ab4aab8b720b3c3e0
CrugBarat <craigtbarratt@gmail.com>
2020-06-24 02:19:23 +0100

    adds nav button to homepage
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,17 @@
 import React, {Fragment} from 'react';
+import NavButton from '../components/NavButton';
+import forward from '../assets/images/forward.png';
 import './Views.css'
+
 
 export default function Home() {
 
   return (
     <Fragment>
       <div className="homepage-title-container">
-      <h3 className="homepage-title">TONK.js</h3>
+        <h3 className="homepage-title">TONK.js</h3>
       </div>
+      <NavButton name="forward" link={"/select"} image={forward} styleName={"forward-button"} />
     </Fragment>
   )
 }
